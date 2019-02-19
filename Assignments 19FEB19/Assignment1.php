a43dd94d926ad7a4ee9d0319db8ad41966de90f6
DannaCb5 <dannal@bellsouth.net>
2019-02-19 15:51:15 -0500

    first commit

---
+++ Assignments 19FEB19/Assignment1.php
@@ -0,0 +1,13 @@
+<html>
+<header></header>
+<body>
+
+<form action="<?php echo htmlspecialchars($_SERVER[]);?>"> method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="email" name="email"><br>
+Website: <input type="url" name="website">
+<input type="submit">
+</form>
+
+</body>
+</html>
